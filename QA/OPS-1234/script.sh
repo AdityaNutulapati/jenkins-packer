54d71e4e894e277b0d845919c7e6d2202d8c23ce
Nutulapati Sriaditya <AdityaNutulapati>
2023-05-05 14:28:34 +0530

    aditya

--- QA/OPS-1234/script.sh
+++ QA/OPS-1234/script.sh
@@ -2,6 +2,8 @@
 # current_date_time=$(date)
 # echo "Current date and time: $current_date_time"
 # cp /test.txt /ami.txt
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+# wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+# sudo apt update && sudo apt install terraform
+sudo apt-get update
+sudo apt-get install -y awscli
