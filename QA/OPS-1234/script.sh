66227dcc851e326b8770dc8bd0434af3a830ab12
Nutulapati Sriaditya <AdityaNutulapati>
2023-05-05 09:42:04 +0530

    aditya

--- QA/OPS-1234/script.sh
+++ QA/OPS-1234/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-current_date_time=$(date)
-echo "Current date and time: $current_date_time"
+# current_date_time=$(date)
+# echo "Current date and time: $current_date_time"
+cp ./test.txt ./ami.txt
